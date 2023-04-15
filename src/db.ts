9daf2cba6ba6b67bd4170e965f234dfbcadca292
Amey <null>
2023-04-15 15:40:00 +0100

    beautify

--- src/db.ts
+++ src/db.ts
@@ -1,75 +1,92 @@
-//Create a promise based IndexDB API 
+//Create a promise based IndexDB API
 
-
-function createObjectStoreFromJSON(db :IDBDatabase,storeJson :Record<string,unknown>,storeName :string) {
-const store = db.createObjectStore(storeName,{keyPath:'key'})
-Object.keys(storeJson).forEach((key)=>{
-    store.createIndex(key,key,{unique:false})
-})
+function createObjectStoreFromJSON(
+  db: IDBDatabase,
+  storeJson: Record<string, unknown>,
+  storeName: string
+) {
+  const store = db.createObjectStore(storeName, { keyPath: "key" });
+  Object.keys(storeJson).forEach((key) => {
+    store.createIndex(key, key, { unique: false });
+  });
 }
 
-export function opendatabase (dbName : string ,version : number ,store : Record<string,unknown>,storeName : string    ):Promise<IDBDatabase> {
-return new Promise((resolve,reject)=>{
-    let db : IDBDatabase;
-    const request = indexedDB.open(dbName,version)
-    request.onerror = ()=>{
-        console.error("Rejeected While Opening up DB")
-        reject(request.error);
-    }
-    request.onsuccess = ()=>{
-        resolve(request.result)
-    }
-    request.onupgradeneeded = () =>{
-        const db = request.result;
-        createObjectStoreFromJSON(db,store,storeName)
-    }
-})
-}
-
-export async function readDatabaseKey(db :IDBDatabase,key : string,storeName:string) :Promise<unknown>{
-   const transaction = db.transaction(storeName,'readonly')
-   const store = transaction.objectStore(storeName)
-   const request =  store.get(key)
-   return new Promise((resolve,reject)=>{
+export function opendatabase(
+  dbName: string,
+  version: number,
+  store: Record<string, unknown>,
+  storeName: string
+): Promise<IDBDatabase> {
+  return new Promise((resolve, reject) => {
+    let db: IDBDatabase;
+    const request = indexedDB.open(dbName, version);
     request.onerror = () => {
-        reject(request.error)
-    }
-    request.onsuccess = ()=>{
-        resolve(request.result?.value)
-    }
-   })
+      console.error("Rejeected While Opening up DB");
+      reject(request.error);
+    };
+    request.onsuccess = () => {
+      resolve(request.result);
+    };
+    request.onupgradeneeded = () => {
+      const db = request.result;
+      createObjectStoreFromJSON(db, store, storeName);
+    };
+  });
 }
 
-export async function readDatabaseStore(dbName:string,storeName:string):Promise<unknown>{
-    return new Promise((resolve,reject)=>{
-    const dbRequest = indexedDB.open(dbName)
-    dbRequest.onsuccess = ()=>{ 
-     const db = dbRequest.result      
-    const transaction = db.transaction(storeName,'readonly')
-    const store = transaction.objectStore(storeName)
-    const resultRq = store.getAll()
-    resultRq.onerror = ()=>{
-        reject(resultRq.error)
-    }
-    resultRq.onsuccess = ()=>{
-        resolve(resultRq.result)
-    }
-    }
-    })
+export async function readDatabaseKey(
+  db: IDBDatabase,
+  key: string,
+  storeName: string
+): Promise<unknown> {
+  const transaction = db.transaction(storeName, "readonly");
+  const store = transaction.objectStore(storeName);
+  const request = store.get(key);
+  return new Promise((resolve, reject) => {
+    request.onerror = () => {
+      reject(request.error);
+    };
+    request.onsuccess = () => {
+      resolve(request.result?.value);
+    };
+  });
 }
 
-export async function saveDatabase(db:IDBDatabase,reduxStore:Record<string,unknown>,storeName:string):Promise<unknown>{
-    const transaction = db.transaction(storeName,'readwrite')
-    const store = transaction.objectStore(storeName)
-    const request = store.put(reduxStore)
-    return new Promise((resolve,reject)=>{
-        request.onerror = ()=>{
-            reject(request.error)
-        }
-        request.onsuccess = ()=>{
-            resolve(request.result)
-        }
-    })
+export async function readDatabaseStore(
+  dbName: string,
+  storeName: string
+): Promise<unknown> {
+  return new Promise((resolve, reject) => {
+    const dbRequest = indexedDB.open(dbName);
+    dbRequest.onsuccess = () => {
+      const db = dbRequest.result;
+      const transaction = db.transaction(storeName, "readonly");
+      const store = transaction.objectStore(storeName);
+      const resultRq = store.getAll();
+      resultRq.onerror = () => {
+        reject(resultRq.error);
+      };
+      resultRq.onsuccess = () => {
+        resolve(resultRq.result);
+      };
+    };
+  });
 }
 
-
+export async function saveDatabase(
+  db: IDBDatabase,
+  reduxStore: Record<string, unknown>,
+  storeName: string
+): Promise<unknown> {
+  const transaction = db.transaction(storeName, "readwrite");
+  const store = transaction.objectStore(storeName);
+  const request = store.put(reduxStore);
+  return new Promise((resolve, reject) => {
+    request.onerror = () => {
+      reject(request.error);
+    };
+    request.onsuccess = () => {
+      resolve(request.result);
+    };
+  });
+}
--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,43 @@
-import {Middleware} from 'redux';
-import { opendatabase, saveDatabase,readDatabaseStore } from './db';
+import { Middleware } from "redux";
+import { opendatabase, saveDatabase, readDatabaseStore } from "./db";
 
-let db :IDBDatabase | undefined = undefined
-let gbStoreName : string | undefined = undefined;
+let db: IDBDatabase | undefined = undefined;
+let gbStoreName: string | undefined = undefined;
 
-const createDB = async (dbName:string,version:number,store:Record<string,unknown>,storeName:string)=>{
-    gbStoreName = storeName
-   db =  await opendatabase(dbName,version,store,gbStoreName)
-}
+const createDB = async (
+  dbName: string,
+  version: number,
+  store: Record<string, unknown>,
+  storeName: string
+) => {
+  gbStoreName = storeName;
+  db = await opendatabase(dbName, version, store, gbStoreName);
+};
 
-const dbMiddleWare:Middleware = (store) => (next) => (action) =>{
-    const result = next(action)
-    const state = store.getState()
-    if(db != undefined && gbStoreName != undefined){
-    saveDatabase(db,state,gbStoreName)
-    }else{
-        console.error('Cant Save as dn and gbStoreName is undefined')
-    }
-    return result
-}
+const dbMiddleWare: Middleware = (store) => (next) => (action) => {
+  const result = next(action);
+  const state = store.getState();
+  if (db != undefined && gbStoreName != undefined) {
+    saveDatabase(db, state, gbStoreName);
+  } else {
+    console.error("Cant Save as dn and gbStoreName is undefined");
+  }
+  return result;
+};
 
-const hydrateReduxFromDB  = async (dbName : string,storeName:string)=>{
-   const hyDrateAction = (data:unknown) =>({
-    type:'HYDRATE_STORE_DB',
-    payload:data
-   })
-   let data = await readDatabaseStore(dbName,storeName)
-   return [data , hyDrateAction(data)]
-}
+const hydrateReduxFromDB = async (dbName: string, storeName: string) => {
+  const hyDrateAction = (data: unknown) => ({
+    type: "HYDRATE_STORE_DB",
+    payload: data,
+  });
+  let data = await readDatabaseStore(dbName, storeName);
+  return [data, hyDrateAction(data)];
+};
 
-const addTabsListner = (cb:()=>{})=>{
-    if(document != undefined){
-        document.addEventListener('visibilitychange',cb)
-    }
-}
+const addTabsListner = (cb: () => {}) => {
+  if (document != undefined) {
+    document.addEventListener("visibilitychange", cb);
+  }
+};
 
-export {
-    createDB,dbMiddleWare,hydrateReduxFromDB,addTabsListner
-}
+export { createDB, dbMiddleWare, hydrateReduxFromDB, addTabsListner };
