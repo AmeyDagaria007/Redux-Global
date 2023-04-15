7b7f48ee55c03865312ff42b408905b01c7195ef
Amey <null>
2023-04-15 14:29:59 +0100

    add index file

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,25 @@
 import {Middleware} from 'redux';
+import { opendatabase, saveDatabase } from './db';
 
+let db :IDBDatabase | undefined = undefined
+let gbStoreName : string | undefined = undefined;
+
+const createDB = async (dbName:string,version:number,store:Record<string,unknown>,storeName:string)=>{
+    gbStoreName = storeName
+   db =  await opendatabase(dbName,version,store,gbStoreName)
+}
+
+const dbMiddleWare:Middleware = (store) => (next) => (action) =>{
+    const result = next(action)
+    const state = store.getState()
+    if(db != undefined && gbStoreName != undefined){
+    saveDatabase(db,state,gbStoreName)
+    }else{
+        console.error('Cant Save as dn and gbStoreName is undefined')
+    }
+    return result
+}
+
+export {
+    createDB,dbMiddleWare
+}
