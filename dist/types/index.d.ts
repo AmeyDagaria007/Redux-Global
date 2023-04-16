c65d90401c1f0f5b3312b2bfd254911702c4b493
Amey <null>
2023-04-16 15:55:26 +0100

    new build

--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -1,5 +1,5 @@
 import { Middleware } from "redux";
-declare const createDB: (dbName: string, version: number, store: Record<string, unknown>, storeName: string) => Promise<void>;
+declare const createDB: (dbName: string, version: number, storeName: string) => Promise<void>;
 declare const dbMiddleWare: Middleware;
 declare const hydrateReduxFromDB: (dbName: string, storeName: string) => Promise<unknown[]>;
 declare const addTabsListner: (cb: () => {}) => void;
